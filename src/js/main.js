e9c9d9cfc7ec9192145cd301e95ec6cd7d2265e6
Daniel San <d.blem@freenet.de>
2023-06-26 10:04:00 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+let currentPokemon;
+
+async function loadPokemon(){
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander'
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    console.log('Das Pokemon ist', currentPokemon)
+    renderPokemonInfo()
+}
+
+function renderPokemonInfo(){
+document.getElementById('pokemon-name').innerHTML =currentPokemon['name']
+document.getElementById('pokemon-img').src = currentPokemon['sprites']['front_default']
+}
