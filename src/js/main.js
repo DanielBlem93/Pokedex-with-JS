e13af89587f68c5d50a60635a0cc84de251892ea
Daniel San <d.blem@freenet.de>
2023-06-29 10:37:41 +0200

    Added commentarys for each function

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,24 @@ let currentPokemon;
 let currentPokemon2;
 
 
-function init(){
+// Initalise all functions onload 
+function init() {
     render()
 }
-
+// renders the HTML cards
+let pokemoncounter = 50
 async function render() {
 
     pokedex.innerHTML = '';
 
-     for (let i = 1; i < 50; i++) {
+    for (let i = 1; i < pokemoncounter; i++) {
 
         await loadPokemon(i)
 
         let pokedex = document.getElementById('pokedex')
         pokedex.innerHTML += /*html*/` 
 
-        <div onclick="togglePopup('flex'), renderInformationCard(${i})" class="pokemon-card bg${i}">
+        <div onclick="togglePopup('flex'), renderInformationCard(${i}),toggleNoScroll('add')" class="pokemon-card bg${i}">
             <div class="image-frame">
                 <img class="pokemon-img-style pokemon-img${i}" src="">
             </div>
@@ -38,16 +40,16 @@ async function render() {
                 </div>
             </div>
         </div>`
-        renderMainData(i,0)
+        renderMainData(i, 0)
     }
 }
+// Fills the HTML Cards with all basic Data
+function renderMainData(i, j) {
+    renderPokemonInfo(i, j)
+    setElement(i, j)
 
-function renderMainData(i,j){
-    renderPokemonInfo(i,j)
-    setElement(i,j)
- 
 }
-
+// loads the data from the API, convert it to a jason and save it in currentpokemon
 async function loadPokemon(i) {
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`
     let url2 = `https://pokeapi.co/api/v2/pokemon-species/${i} `
@@ -58,80 +60,60 @@ async function loadPokemon(i) {
     currentPokemon2 = await response2.json();
     console.log('Das Pokemon ist', currentPokemon)
     console.log('Siehe da:', currentPokemon2)
-
 }
 
-async function renderPokemonInfo(i,j) {
+// Gives the Pokemoncard the Name, ID and the Image
+async function renderPokemonInfo(i, j) {
     let id = document.getElementsByClassName(`pokemon-number${i}`)[j]
     let image = document.getElementsByClassName(`pokemon-img${i}`)[j]
     let name = document.getElementsByClassName(`pokemon-name${i}`)[j]
-    
-    id.innerHTML =  currentPokemon.id
-    image.src =  currentPokemon['sprites']['other']['official-artwork']['front_default']
-    name.innerHTML =  currentPokemon2['names']['5']['name']
+
+    id.innerHTML = currentPokemon.id
+    image.src = currentPokemon['sprites']['other']['official-artwork']['front_default']
+    name.innerHTML = currentPokemon2['names']['5']['name']
 }
 
-function setElement(i,j){
+// Gives the Pokemon there Elements 
+function setElement(i, j) {
 
     let element1 = document.getElementsByClassName(`element1${i}`)
     let element2 = document.getElementsByClassName(`element2${i}`)
-    let type1 =  currentPokemon['types'][0]['type']['name']
+    let type1 = currentPokemon['types'][0]['type']['name']
     let type2
 
     element1[j].innerHTML = type1
 
-    if(currentPokemon['types'][1]){
+    if (currentPokemon['types'][1]) {
         type2 = currentPokemon['types'][1]['type']['name']
         element2[j].innerHTML = type2
-    }else{
+    } else {
         element2[j].style.display = 'none'
     }
-  changeBackgroundColor(i,j)
+    changeBackgroundColor(i, j)
 }
 
-async function loadDescriptions(){
-    let description = currentPokemon2['flavor_text_entries']['25']['flavor_text']
-    let content =document.getElementsByClassName ('description')[0]
-
-    content.innerHTML = ""
-    content.innerHTML = description
-}
-
-function changeBackgroundColor(i,j){
+// Gives the Pokemon the right backgroundcolor based on there Element
+function changeBackgroundColor(i, j) {
     let pokemonCard = document.getElementsByClassName(`bg${i}`)
     let mainElement = document.getElementsByClassName(`element1${i}`)[j].innerHTML
     let secondElement = document.getElementsByClassName(`element2${i}`)[j].innerHTML
-    
-    let element1= document.getElementsByClassName(`element1${i}`)[j]
-    let element2= document.getElementsByClassName(`element2${i}`)[j]
 
-    pokemonCard[j].classList.add(`${mainElement}`) 
-    element1.classList.add(`${mainElement}`) 
-    element2.classList.add(`${secondElement}`) 
+    let element1 = document.getElementsByClassName(`element1${i}`)[j]
+    let element2 = document.getElementsByClassName(`element2${i}`)[j]
 
+    pokemonCard[j].classList.add(`${mainElement}`)
+    element1.classList.add(`${mainElement}`)
+    element2.classList.add(`${secondElement}`)
 }
 
-function togglePopup(display){
-    let informationCard = document.getElementById('information-card')
-    informationCard.style.display = `${display}`
+// All functions for the Popup Information Card
 
-}
-  
-function doNotClose(event){
-event.stopPropagation()
-}
+// Renders the HTML popup Informationcard
+async function renderInformationCard(i) {
 
-function toggleNoScroll(action){
-let body =document.getElementsByTagName('body')[0]
-body.classList[action]('noscroll')
-
-}
-
-async function renderInformationCard(i){
-
-let informationContainer = document.getElementById('information-container')
-informationContainer.innerHTML ="";
-informationContainer.innerHTML = /*html*/`
+    let informationContainer = document.getElementById('information-container')
+    informationContainer.innerHTML = "";
+    informationContainer.innerHTML = /*html*/`
 
       
     <img onclick="togglePopup('none'), toggleNoScroll('remove')" id="close" src="src/img/close.png">
@@ -174,92 +156,34 @@ informationContainer.innerHTML = /*html*/`
     </div>
     
 `
-await loadPokemon(i)
-renderMainData(i,1)
-loadDescriptions()
+    await loadPokemon(i)
+    renderMainData(i, 1)
+    loadDescriptions()
 }
 
+// loads the Description text for the Pokemon and add the HTML
+async function loadDescriptions() {
+    let description = currentPokemon2['flavor_text_entries']['25']['flavor_text']
+    let content = document.getElementsByClassName('description')[0]
 
+    content.innerHTML = ""
+    content.innerHTML = description
+}
 
+// Handels the function to close and open the Information Card
+function togglePopup(display) {
+    let informationCard = document.getElementById('information-card')
+    informationCard.style.display = `${display}`
 
+}
+//   Avoid that the Popup close when you click on it
+function doNotClose(event) {
+    event.stopPropagation()
+}
+// avoid scrolling while popup is open
+function toggleNoScroll(action) {
+    let body = document.getElementsByTagName('body')[0]
+    body.classList[action]('noscroll')
 
+}
 
-
-
-// 'normal' 'grass' 'poisen' 'fire' 
-// 'flying' 'water' 'bug' 'electric' 'ground' 'fairy' 
-// 'fighting' 'psychic' 'rock' 'steel' 'ice' 'ghost' 'dragon'
-
-// function changeBgColor(i, color){
-// let pokemonCard = document.getElementsByClassName(`bg${i}`)[0];
-// pokemonCard.classList.add('')
-// }
-
-
-// async function renderPokemonInfo(i) {
-//     let id = document.getElementsByClassName(`pokemon-number${i}`)[0]
-//     let image = document.getElementById(`pokemon-img${i}`)
-//     let name = document.getElementsByClassName(`pokemon-name${i}`)[0]let
-//  
-//      let color = currentPokemon2['color']['name']
-        // let text = currentPokemon2['flavor_text_entries']['25']['flavor_text']
-//     id.innerHTML =  currentPokemon.id
-//     image.src =  currentPokemon['sprites']['other']['official-artwork']['front_default']
-//     name.innerHTML =  currentPokemon2['names']['5']['name']
-
-// }
- 
-
- // switch (element) {
-    //     case 'normal':
-    //         pokemonCard[0].classList.add('normal');
-    //         break;
-    //     case 'grass':
-    //         pokemonCard[0].classList.add('grass');
-    //         break;
-    //     case 'poison':
-    //         pokemonCard[0].classList.add('poison');
-    //         break;
-    //     case 'fire':
-    //         pokemonCard[0].classList.add('fire');
-    //         break;
-    //     case 'flying':
-    //         pokemonCard[0].classList.add('flight');
-    //         break;
-    //     case 'water':
-    //         pokemonCard[0].classList.add('water');
-    //         break;
-    //     case 'bug':
-    //         pokemonCard[0].classList.add('bug');
-    //         break;
-    //     case 'electric':
-    //         pokemonCard[0].classList.add('electric');
-    //         break;
-    //     case 'ground':
-    //         pokemonCard[0].classList.add('ground');
-    //         break;
-    //     case 'fairy':
-    //         pokemonCard[0].classList.add('fairy');
-    //         break;
-    //     case 'fighting':
-    //         pokemonCard[0].classList.add('fighting');
-    //         break;
-    //     case 'psychic':
-    //         pokemonCard[0].classList.add('psychic');
-    //         break;
-    //     case 'rock':
-    //         pokemonCard[0].classList.add('ground');
-    //         break;
-    //     case 'steel':
-    //         pokemonCard[0].classList.add('steel');
-    //         break;
-    //     case 'ice':
-    //         pokemonCard[0].classList.add('ice');
-    //         break;
-    //     case 'ghost':
-    //         pokemonCard[0].classList.add('ghost');
-    //         break;
-    //     case 'dragon':
-    //         pokemonCard[0].classList.add('dragon');
-    //         break;
-    // }
