f1c6fd50445732188f3dab2d21b8d6cf5fd862f3
Daniel San <d.blem@freenet.de>
2023-07-05 18:00:25 +0200

    added a function to disable arrows while searching

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -21,25 +21,43 @@ function activeReiter(action1, action2, action3) {
 // Handels the function to close and open the Information Card
 function togglePopup(display) {
     let informationCard = document.getElementById('information-card')
-   
-   
+
+
     switch (display) {
         case 'none':
-            informationCard.style.opacity= '0'
+            informationCard.style.opacity = '0'
             setTimeout(() => {
                 informationCard.style.display = `${display}`
+                disableArrows()
             }, 300);
             break;
-    
+
         case 'flex':
-           
+
             informationCard.style.display = `${display}`
             setTimeout(() => {
                 informationCard.style.opacity = '1'
+                disableArrows()
             }, 100);
+       
             break;
     }
+   
+}
 
+function disableArrows() {
+   
+    let arrowRight = document.getElementById('right-arrow')
+    let arrowLeft = document.getElementById('left-arrow')
+    if (resetCounter == 1) {
+        arrowLeft.style.display = 'none'
+        arrowRight.style.display = 'none'
+    } else if ( resetCounter == 0) {
+        arrowLeft.style.display = 'unset'
+        arrowRight.style.display = 'unset'
+    }else{
+        console.log('nothing')
+    }
 }
 
 //   Avoid that the Popup closes when you click on it
